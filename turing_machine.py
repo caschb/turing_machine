3afd480617d98b9a2e8437c09de49e4f7b2572f5
= <jostin.alvarez@ucr.ac.cr>
2020-07-18 20:00:46 -0600

    Added documentation to the turing_machine and turing_machine_multitape classes

--- turing_machine.py
+++ turing_machine.py
@@ -42,8 +42,8 @@
 #
 #-------------------------------------------------------------------------------------------------------
 #   2. Implementation remarks
-#   a. Current version: There are two TMs: a deterministic and a nondeterministic one. Tapes are 
-#       implemented as as lists and displayed as strings.   
+#   a. Current version: There are two TMs: a single taped turing machine and a multitape one. Tapes are 
+#       implemented as as lists and displayed as strings.     
 #
 
 import enum # Allows for the usage of enumerators, which have codes for the possible results after 
@@ -82,92 +82,124 @@ class TuringMachine:
         OUTSIDE = 2 # If the input string requires more space than the given max_length
         UNDECIDABLE = 3 # If the input string requires more steps than given max_steps to be decided
 
+    """ Loads the definition of the machine with the format specified on the project instructions """
     def load_machine_definition(self, filename):
-        """ Loads the definition of the machine with the format specified on the assigment """
 
+        # open the file to read the configurations
         with open(filename) as f:
             line = f.readline()
+
+            # Separate the string with comma as a sepparator, as the file comes in the format (name, numberofTapas, maxlenght, maxsteps)
             elements = line.strip().split(',')
+            
+            # Assign the values to the class attributes
             self._name, self._max_length, self._max_steps = elements[0], int(elements[1]), int(elements[2])
 
             line = f.readline()
-            self._states = line.strip().split(',')
+            self._states = line.strip().split(',') # The states come in the format "q1,q2,q3,q4,..."
             
             line = f.readline()
-            self._input_alphabet = line.strip().split(',')
+            self._input_alphabet = line.strip().split(',') # The alphabet comes in the format "acceptedCharacter1, accepterCharacter2,...""
             
             line = f.readline()
-            self._tape_alphabet = line.strip().split(',')
+            self._tape_alphabet = line.strip().split(',') # Alphabet of the tape
 
             line = f.readline()
-            self._initial_state = line.strip()
+            self._initial_state = line.strip() # REad the initial state
 
             line = f.readline()
-            self._accept_state, self._reject_state = line.strip().split(',')
+            self._accept_state, self._reject_state = line.strip().split(',') # Read the accept and reject states
             
+            # Read all the transitions available in the turing machine
             line = f.readline()
             while(line):
                 self._transitions.append(line.strip().split(','))
                 line = f.readline()
-                        
+                 
+        # Set the start state       
         self._current_state = self._initial_state
+
+        # Start the _tape in Null for each symbol
         self._tape = [None for i in range(self._max_length)]
 
+        # Always add the blank symbol to the input alphabet
         self._input_alphabet.append('_')
 
+    """ Loads the initial strings from a file """
     def load_initial_strings(self, filename):
-        """ Loads the initial strings from a file """
+
+        # Opens the file that has the strings of a language to see if it is recognized by the TM
         with open(filename) as f:
             line = f.readline()
+
+            # Read each of the transitions
             while(line):
                 stripped_string = line.strip()
                 if(len(stripped_string) > 0 ):
                     self._initial_strings.append(list(stripped_string))
                 line = f.readline()
 
+    """Cleans the TM current configuration"""
     def _clean(self):
-        """Cleans the TM current configuration"""
+
+        # Make the machine be in the initial state again
         self._current_state = self._initial_state
+
+        # Reset the index and the head position of the tape
         self._tape = [None for i in range(self._max_length)]
         self._current_step = 0
         self._head_position = 0
 
+
+    """Fills the tape with the input string"""
     def _init_tape(self):
-        """Fills the tape with the input string"""
         self._tape = [None for i in range(self._max_length)]
         initial_string = self._initial_strings.pop(0)
         return_character = '\0'
-        for index, character in enumerate(initial_string):
-            if (character not in self._input_alphabet):
-                return character, True
 
+        # Checks all the characters in the string
+        for index, character in enumerate(initial_string):
+            # Checks if the character is not in the alphabet
+            if (character not in self._input_alphabet):
+                return character, True # Returns the character and an error
+
+            # The character was valid, add it to the list
             self._tape[index] = character
             return_character = character
             
+        # No error was found, return the character and False
         return return_character, False
             
 
+    # Method that finds transitions from current state to others.
     def _decode(self):
-        """Search for the rule that applies to the current symbol"""
         current_values = list([self._current_state, self._tape[self._head_position]])
+        
+        # Search through all the transtitions to check if one is valid from current state and symbol
         for rule  in self._transitions:
+            # Checks if the current symbol in the tape matches the one in the transition, to check if they match
             if rule[0] == current_values[0] and rule[1] == current_values[1]:
                 return rule
         return None
 
+
+    """"Applies the rule, moves the TMhead to the left or right"""
     def _execute(self, current_rule, quiet=False):
-        """"Applies the rule, moves the TMhead to the left or right"""
         self._current_state = current_rule[2]
         self._tape[self._head_position] = current_rule[3]
-        if(current_rule[-1] == 'R'):
+
+        if(current_rule[-1] == 'R'): # Right
             self._head_position += 1
-        elif(current_rule[-1] == 'L'):
+        elif(current_rule[-1] == 'L'): # Left
             self._head_position -= 1
         if not quiet:
             self._print_step()
     
+
+    """Checks whats the final state of the TM"""
     def _verify(self):
-        """Checks whats the final state of the TM"""
+
+        # Uses the enumerate to set the final state
         if self._current_state == self._accept_state:
             return True, self.FinalState.ACCEPTED
         elif self._current_state == self._reject_state:
@@ -179,8 +211,9 @@ class TuringMachine:
         else:
             return False,-1
 
+
+    """Prints the tape with its respective state"""
     def _print_step(self):
-        """Prints the tape with its respective state"""
         output_string = ""
         for idx, character in enumerate(self._tape):
             if(character == None):
@@ -190,26 +223,40 @@ class TuringMachine:
             output_string += f" {character} "
         print(output_string)
 
+
+    """Gets the next string, loads it into the TM tape and process it."""
     def run(self,quiet=False):
-        """Gets the next string, loads it into the TM tape and process it."""
+        # Loop while there are strings to read
         while(len(self._initial_strings) > 0):
+            # Calls the method to restart the variables
             self._clean()
+
+            # Starts the turing machine, and stores if any error was found
             character, error = self._init_tape()
 
+            # Detects that a invalid character was entered and prints an error message
             if(error):
                 print("Caracter inválido: \'" + character + "\'")
-            else:
+            else: # If everything worked well initializing the TM
                 stop = False
                 reason = -1
+
+                # Loop as long as the machine is not accepted, rejected or reached max steps
                 while(not stop):
+                    # Calls the method to find a transition from the current state
                     current_rule = self._decode()
+
+                    # No rule was found from this state to another, reject
                     if(current_rule == None):
                         reason = self.FinalState.REJECTED
                         break
                     self._execute(current_rule, quiet)
+
+                    # Calls the method to find transitions and check if the machine terminated (reason)
                     stop, reason = self._verify()
                     self._current_step += 1
 
+                # Detects if the string was accepted, rejected, or other
                 if reason == self.FinalState.ACCEPTED:
                     print("Aceptado")
                 elif reason == self.FinalState.REJECTED:
@@ -222,7 +269,9 @@ class TuringMachine:
                     print("Rechazado")
 
 
+    # Method to print the turing machine
     def __str__(self):
+        # Creates a string with name, lenght, steps and states
         output_string = f"Name: {self._name}\n"\
                         f"Max length: {self._max_length}\n"\
                         f"Max steps: {self._max_steps}\n" \
@@ -232,10 +281,14 @@ class TuringMachine:
                         f"Q_o: {self._initial_state}\n" \
                         f"Q_accept, Q_reject: {self._accept_state}, {self._reject_state}\n" \
                         f"δ: \n"
+        
+        # Adds the transitions to the string
         for element in self._transitions:
             output_string += f"{element}\n"
         
         output_string += "----TEST STRINGS:----\n"
+
+        # Prints the strings that were passed to the TM
         for element in self._initial_strings:
             output_string += f"{element}\n"
 
--- turing_machine_multitape.py
+++ turing_machine_multitape.py
@@ -42,16 +42,18 @@
 #
 #-------------------------------------------------------------------------------------------------------
 #   2. Implementation remarks
-#   a. Current version: There are two TMs: a deterministic and a nondeterministic one. Tapes are 
+#   a. Current version: There are two TMs: a single taped turing machine and a multitape one. Tapes are 
 #       implemented as as lists and displayed as strings.   
 #
 
 import enum # Allows for the usage of enumerators, which have codes for the possible results after 
             # a turing machine runs through an input String
 import os    
+
 ########################################################################################################
-# TuringMachine class. Contains all the methods in order to create a Turing machine based on a config
-# file and run all the strings input strings, deciding (or not) from a input strings file. 
+# MultitapeTuringMachine class. Contains all the methods in order to create a Turing machine with multiple
+# tapes and run test strings based on the config files.
+#
 class MultitapeTuringMachine:
     """ Contains the internal configuration of the machine and functions to run the configuration """
 
@@ -84,112 +86,149 @@ class MultitapeTuringMachine:
         OUTSIDE = 2 # If the input string requires more space than the given max_length
         UNDECIDABLE = 3 # If the input string requires more steps than given max_steps to be decided
 
+    """ Loads the definition of the machine with the format specified on the project instructions """
     def load_machine_definition(self, filename):
-        """ Loads the definition of the machine with the format specified on the assigment """
 
+        # open the file to read the configurations
         with open(filename) as f:
             line = f.readline()
+
+            # Separate the string with comma as a sepparator, as the file comes in the format (name, numberofTapas, maxlenght, maxsteps)
             elements = line.strip().split(',')
+
+            # Assign the values to the class attributes
             self._name, self._number_of_tapes, self._max_length, self._max_steps = elements[0], int(elements[1]), int(elements[2]), int(elements[3])
 
             line = f.readline()
-            self._states = line.strip().split(',')
+            self._states = line.strip().split(',') # The states come in the format "q1,q2,q3,q4,..."
             
             line = f.readline()
-            self._input_alphabet = line.strip().split(',')
+            self._input_alphabet = line.strip().split(',') # The alphabet comes in the format "acceptedCharacter1, accepterCharacter2,...""
             
             # Alphabet for each tape
             for alphabet in range(self._number_of_tapes):
                 line = f.readline()
                 self._tapes_alphabet.append(line.strip().split(','))
 
+            # Read the initial State
             line = f.readline()
             self._initial_state = line.strip()
 
+            # Read the accept and reject states
             line = f.readline()
             self._accept_state, self._reject_state = line.strip().split(',')
             
+            # Read all the transitions available in the turing machine
             line = f.readline()
             while(line):
                 self._transitions.append(line.strip().split(','))
                 line = f.readline()
                         
+        # Set the start state
         self._current_state = self._initial_state
         
+        # Go through each of the tapes 
         for tape_index in range(self._number_of_tapes):
             self._head_positions.append(0)
             self._tapes.append([ "_" for i in range(self._max_length)])
+
+        # Always add the blank symbol to the input alphabet
         self._input_alphabet.append('_')
 
+
+    """ Loads the initial strings from a file """
     def load_initial_strings(self, filename):
-        """ Loads the initial strings from a file """
+
+        # Opens the file that has the strings of a language to see if it is recognized by the TM
         with open(filename) as f:
             line = f.readline()
+
+            # Read each of the transitions
             while(line):
                 stripped_string = line.strip()
                 if(len(stripped_string) > 0 ):
                     self._initial_strings.append(list(stripped_string))
                 line = f.readline()
 
+
+    """Cleans the TM current configuration"""
     def _clean(self):
-        """Cleans the TM current configuration"""
+        # Make the machine be in the initial state again
         self._current_state = self._initial_state
+
+        # Reset the indexes and the head position of each tape
         for tape_index in range(self._number_of_tapes):
             self._tapes[tape_index] = ["_" for i in range(self._max_length)]
             self._head_positions[tape_index] = 0
+
+        # Restart the current step 
         self._current_step = 0
 
+    """Fills the tape with the input string"""
     def _init_tape(self):
-        """Fills the tape with the input string"""
         self._tapes[0] = ["_" for i in range(self._max_length)]
         initial_string = self._initial_strings.pop(0)
         return_character = '\0'
-        for index, character in enumerate(initial_string):
-            if (character not in self._input_alphabet):
-                return character, True
 
+        # Checks all the characters in the string
+        for index, character in enumerate(initial_string):
+
+            # Checks if the character is not in the alphabet
+            if (character not in self._input_alphabet):
+                return character, True # Returns the character and an error
+
+            # The character was valid, add it to the list
             self._tapes[0][index] = character
             return_character = character
             
+        # No error was found, return the character and False
         return return_character, False
             
 
+    # Method that finds transitions from current state to others.
     def _decode(self):
-       # Comparar si el estado igual es igual al de la regla, si todos los
-       # valores de simbolos iniciales hacen match en el tape.
+        # Compare if the initial state is equal to the transition, if all
+        # values of the initial symbols match with the tape
         for rule in self._transitions:
+
+            # Checks if the transition applies to the current state
             if (rule[0] == self._current_state):
                 rule_applies = True
+
                 # For every initial symbol in each tape
                 for initial_symbols in range(1, (self._number_of_tapes + 1)):
+                    # Checks if the current symbol in the tape matches the one in the transition, to check if they match
                     if (self._tapes[initial_symbols - 1][self._head_positions[initial_symbols-1]] != rule[initial_symbols]):
                         rule_applies = False
                 if (rule_applies):
                     return rule        
         return None
 
+    """"Applies the rule, moves the TMhead to the left or right"""
     def _execute(self, current_rule, quiet=False):
-        """"Applies the rule, moves the TMhead to the left or right"""
+        
         final_state_position = self._number_of_tapes + 1
         self._current_state = current_rule[final_state_position]
 
+        # Goes through each of the tapes
         for tape_index in range(self._number_of_tapes):
             final_state_position +=1
         
+            # Checks where the tape has to move
             self._tapes[tape_index][self._head_positions[tape_index]] = current_rule[final_state_position]
-            if(current_rule[final_state_position + self._number_of_tapes ] == 'r'):
+            if(current_rule[final_state_position + self._number_of_tapes ] == 'r'): # right 
                 self._head_positions[tape_index] += 1
-            elif(current_rule[final_state_position + self._number_of_tapes] == 'l'):
+            elif(current_rule[final_state_position + self._number_of_tapes] == 'l'): # left
                 self._head_positions[tape_index] -= 1
-            elif(current_rule[final_state_position + self._number_of_tapes] == 's'):
+            elif(current_rule[final_state_position + self._number_of_tapes] == 's'): # none
                 pass
             
-        
+        # flag quiet that makes it so prints don't occur
         if not quiet:
             self._print_step()
-    
+
+    """Checks whats the final state of the TM"""
     def _verify(self):
-        """Checks whats the final state of the TM"""
         if self._current_state == self._accept_state:
             return True, self.FinalState.ACCEPTED
         elif self._current_state == self._reject_state:
@@ -201,12 +240,16 @@ class MultitapeTuringMachine:
         else:
             return False,-1
 
+    """Prints the tape with its respective state"""
     def _print_step(self):
-        """Prints the tape with its respective state"""
         print("----STEP----")
+
+        # Go printing each of the tapes
         for tape_idx, dummy in enumerate(self._tapes):
             print(f"Tape: {tape_idx}")
             output_string = ""
+
+            # print each of the characters
             for idx, character in enumerate(self._tapes[tape_idx]):
                 if(character == None):
                     break
@@ -215,26 +258,39 @@ class MultitapeTuringMachine:
                 output_string += f" {character} "
             print(output_string)
 
+    """Gets the next string, loads it into the TM tape and process it."""
     def run(self,quiet=False):
-        """Gets the next string, loads it into the TM tape and process it."""
+        # Loop while there are strings to read
         while(len(self._initial_strings) > 0):
+            # Calls the method to restart the variables
             self._clean()
+
+            # Starts the turing machine, and stores if any error was found
             character, error = self._init_tape()
 
+            # Detects that a invalid character was entered and prints an error message
             if(error):
                 print("Caracter inválido: \'" + character + "\'")
-            else:
+            else: # If everything worked well initializing the TM
                 stop = False
                 reason = -1
+
+                # Loop as long as the machine is not accepted, rejected or reached max steps
                 while(not stop):
+                    # Calls the method to find a transition from the current state
                     current_rule = self._decode()
+
+                    # No rule was found from this state to another, reject
                     if(current_rule == None):
                         reason = self.FinalState.REJECTED
                         break
                     self._execute(current_rule, quiet)
+
+                    # Calls the method to find transitions and check if the machine terminated (reason)
                     stop, reason = self._verify()
                     self._current_step += 1
 
+                # Detects if the string was accepted, rejected, or other
                 if reason == self.FinalState.ACCEPTED:
                     print("Aceptado")
                 elif reason == self.FinalState.REJECTED:
@@ -243,24 +299,22 @@ class MultitapeTuringMachine:
                     print("Fuera")
                 elif reason == self.FinalState.UNDECIDABLE:
                     print("Indecidible")
-                elif reason == self.FinalState.REJECTED:
-                    print("Rechazado")
 
-
+    # Method to print the turing machine
     def __str__(self):
+        # Creates a string with name, lenght, steps and states
         output_string = f"Name: {self._name}\n"\
                         f"Max length: {self._max_length}\n"\
                         f"Max steps: {self._max_steps}\n" \
                         f"Q: {self._states}\n" \
-                        #f"Σ: {self._input_alphabet}\n" \
-                        #f"Γ: {self._tape_alphabet}\n" \
-                        #f"Q_o: {self._initial_state}\n" \
-                        #f"Q_accept, Q_reject: {self._accept_state}, {self._reject_state}\n" \
-                        #f"δ: \n"
+        
+        # Adds the transitions to the string
         for element in self._transitions:
             output_string += f"{element}\n"
         
         output_string += "----TEST STRINGS:----\n"
+
+        # Prints the strings that were passed to the TM
         for element in self._initial_strings:
             output_string += f"{element}\n"
 
